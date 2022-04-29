0d8670c2ddf8ecf9624ef3941c48a856f224e4d5
Tyler Wang <wangtyler123@gmail.com>
2022-04-28 21:16:43 -0500

    proj: initial project setup
    PROJ CONF

---
+++ build/src/CMakeFiles/graph.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../../bin/graph"
+  "../../bin/graph.pdb"
+  "CMakeFiles/graph.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/graph.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
